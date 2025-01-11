e528b39ed0ad2b2ca728ec0921580baae7d3e3ca
Nyasha Shepard Ushewokunze <mrshepard18@gmail.com>
2025-01-11 14:46:21 +0200

    remove port 3000

--- server.js
+++ server.js
@@ -42,9 +42,5 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-const PORT = process.env.PORT || 5500;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
 
